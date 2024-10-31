1e8317702577428e92773af49dc95d446f81614f
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-10-31 15:38:03 +0600

    objects

---
+++ module-1/dist/objects.js
@@ -0,0 +1,8 @@
+"use strict";
+const user = {
+    name: 'jhankar mahbub',
+    country: 'Bd',
+    skill: 'full-stack developer',
+    phone: 2472000,
+    isMerriad: true
+};
---
+++ module-1/src/objects.ts
@@ -0,0 +1,14 @@
+const user:{
+    name:'jhankar mahbub';
+    country:string;
+    readonly skill:string;
+    phone:number;
+    village?:string;
+    isMerriad:boolean
+} = {
+    name:'jhankar mahbub',
+    country:'Bd',
+    skill:'full-stack developer',
+    phone:2472000,
+    isMerriad:true
+}
