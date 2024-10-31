b2b66cc855c5f43c5ac20c0e39b83b05120d3e1c
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-10-31 09:27:17 +0600

    typescript practise

---
+++ module-1/src/basicType.ts
@@ -0,0 +1,27 @@
+// basic data type primitive
+// string
+let fristName:string = 'mehedi'
+// number
+let roll:number = 23
+// boolean
+let isAdmit:boolean = true
+// undefined
+let x:undefined = undefined
+// null
+let y: null=null
+
+let dew:boolean;
+// dew = 'nurmoni'
+// dew = 9
+dew = true
+
+// non-primitive
+
+let frinds:string[] = ['mehedi', 'hasan', 'rana']
+let frindsRoll:number[] = [9, 22, 52]
+
+// tuple --> array --> order --> type of value
+let coordinates:[number, number] = [23, 45];
+let ageName: [number, string] = [24, 'mehedi']
+
+let threeTuple: [boolean, string, number] = [true, 'i can', 1]
---
+++ module-1/src/test.ts
@@ -0,0 +1,1 @@
+const testing='i am testing';
