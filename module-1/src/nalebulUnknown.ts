48474d66556c0644d17df0af986214862aa39f66
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-11-01 10:11:56 +0600

    nelable unknown type dataa

---
+++ module-1/src/nalebulUnknown.ts
@@ -0,0 +1,14 @@
+{
+    function getChecking (value:unknown){
+        if(typeof value === "number"){
+            let result = value * 3;
+            console.log(result)
+        }
+        else if(typeof value === 'string'){
+            console.log('mountain dew')
+        }  else{
+            console.log('wrong value')
+        }
+    }
+    getChecking(null)
+}
